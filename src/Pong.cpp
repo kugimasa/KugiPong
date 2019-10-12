f3d17fd37348b73669c528336cf2593caa464a91
kugi <kugimania@gmail.com>
2019-10-12 16:58:34 +0900

    add Wall, Paddle, Ball

--- src/Pong.cpp
+++ src/Pong.cpp
@@ -1,7 +1,14 @@
 #include "Pong.h"
 
+const int winW = 450;
+const int winH = 640;
+const int thickness = 40;
+const float paddleSize = 300.0f;
+
+
 Pong::Pong()
 :window(nullptr)
+,renderer(nullptr)
 ,isRunning(true)
 {
 
@@ -15,16 +22,28 @@ bool Pong::Initialize()
         SDL_Log("Cannot init SDL: %s", SDL_GetError());
         return false;
     }
-
+    
+    //Create Window
     window = SDL_CreateWindow("KugiPong",
                               100,100,
-                              1024,768,
+                              winW,winH,
                               0);
     if(!window)
     {
         SDL_Log("Cannot create window: %s", SDL_GetError());
         return false;
     }
+    
+    //Create Renderer
+    renderer  = SDL_CreateRenderer(window,
+                                   -1,
+                                   SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    //Initialize parameter
+    paddlePos.x = winW/2.0f;
+    paddlePos.y = winH - thickness;
+    ballPos.x = winW/2.0f;
+    ballPos.y = winH/2.0f;
+    
     return true;
 }
 
@@ -41,6 +60,7 @@ void Pong::RunLoop()
 void Pong::Shutdown()
 {
     SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
     SDL_Quit();
 }
 
@@ -72,6 +92,61 @@ void Pong::UpdateGame()
 
 void Pong::GenerateOutput()
 {
+    //BackGround color
+    SDL_SetRenderDrawColor(renderer,
+                           68,   //R
+                           114,   //G
+                           196, //B
+                           255  //A
+                           );
+    //Clear Buffer with BackGround color
+    SDL_RenderClear(renderer);
     
+    //Wall color
+    SDL_SetRenderDrawColor(renderer, 208, 206, 206, 255);
+    SDL_Rect wall{
+        0,
+        0,
+        winW,
+        thickness
+    };
+    SDL_RenderFillRect(renderer, &wall);
+    
+    //Draw right wall
+    wall.x = 0;
+    wall.y = 0;
+    wall.w = thickness;
+    wall.h = winH;
+    SDL_RenderFillRect(renderer, &wall);
+    
+    //Draw left wall
+    wall.x = winW - thickness;
+    wall.y = 0;
+    wall.w = thickness;
+    wall.h = winH;
+    SDL_RenderFillRect(renderer, &wall);
+    
+    //Paddle
+    SDL_SetRenderDrawColor(renderer, 254, 248, 84, 255);
+    SDL_Rect paddle{
+        static_cast<int>(paddlePos.x - paddleSize/2),
+        static_cast<int>(paddlePos.y),
+        static_cast<int>(paddleSize),
+        thickness
+    };
+    SDL_RenderFillRect(renderer, &paddle);
+    
+    //Ball
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_Rect ball{
+        static_cast<int>(ballPos.x - thickness/2),
+        static_cast<int>(ballPos.y - thickness/2),
+        thickness,
+        thickness
+    };
+    SDL_RenderFillRect(renderer, &ball);
+    
+    //Swap front-buffer and back-buffer
+    SDL_RenderPresent(renderer);
 }
 
--- src/Pong.h
+++ src/Pong.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <SDL2/SDL.h>
 
+struct Vec2
+{
+    float x;
+    float y;
+};
+
 class Pong
 {
 public:
@@ -13,7 +19,11 @@ private:
     void UpdateGame();
     void GenerateOutput();
     
-    // SDL Window
+    //SDL
     SDL_Window* window;
+    SDL_Renderer* renderer;
+    
     bool isRunning;
+    Vec2 paddlePos;
+    Vec2 ballPos;
 };
